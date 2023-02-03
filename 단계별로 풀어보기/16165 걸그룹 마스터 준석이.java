601a2dad7e85da4a64b17f4857d81820649826b6
Elandland <68230815+Elandland@users.noreply.github.com>
2023-02-03 18:18:43 +0900

    Create 16165 걸그룹 마스터 준석이.java

---
+++ 단계별로 풀어보기/16165 걸그룹 마스터 준석이.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashMap<String,String> Hash = new HashMap<String, String>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());           //st로 받아서 int형으로 변환 st의 첫번째 값 호출
+        int M = Integer.parseInt(st.nextToken());           //두번째
+
+
+        for(int i=0; i<N; i++){
+            String group = br.readLine();
+
+            Hash.put();
+            
+        }
+
+    }
+}
